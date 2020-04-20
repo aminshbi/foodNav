1b45cbaa97d9f101869871101208beb0a5b3a471
Amin <aminshbi@gmail.com>
2020-04-20 15:04:11 +0300

    handle navigation and passing parameters between pages

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { View , Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data'
+import Colors from '../constants/Colors'
 
 const CategoryMealsScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+
     return (
         <View style={styles.screen}>
             <Text>Category Meals Screen</Text>
-            <Button title="Go to Details" onPress={()=> {
-                props.navigation.navigate({routeName: 'MealDetail'})
+            <Text>{selectedCategory.title}</Text>
+            <Button title="Go to Details" onPress={() => {
+                props.navigation.navigate({ routeName: 'MealDetail' })
             }} />
-            <Button title="Go back" onPress={()=> {
+            <Button title="Go back" onPress={() => {
                 props.navigation.goBack()
             }} />
         </View>
     )
 }
 
-const  styles = StyleSheet.create({
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+    const catId = navigationData.navigation.getParam('categoryId')
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId)
+    return {
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Colors.primaryColor
+        },
+        headerTintColor: 'white'
+    }
+}
+
+const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
